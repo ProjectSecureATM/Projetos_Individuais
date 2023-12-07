28088ae30d8d19ed1d26e87d04279a54bb69b5f4
Pedro Gomes <pedro.gomes@sptech.school>
2023-12-07 20:41:51 -0300

    alterações caramico

--- Primon/projeto individuas.py
+++ Primon/projeto individuas.py
@@ -2,77 +2,81 @@ import platform
 import datetime
 import mysql.connector
 import psutil
+import pywinusb.hid as hid
+import pymssql
+import time
+
 
 class USBMonitor:
     def __init__(self):
         self.current_os = platform.system()
-        self.conn = mysql.connector.connect(
+        self.conn_mysql = mysql.connector.connect(
             host='localhost',
             user='root',
-            #password='sptech',
-            password='181004Mp.',
+            password='Ph993387998',
             database='SecureATM'
         )
-        self.cursor = self.conn.cursor()
+
+        self.conn_sql_server = pymssql.connect(
+            server='18.204.118.27',
+            database='secureATM',
+            user='sa',
+            password='Secure2023'
+        )
+        self.cursor_mysql = self.conn_mysql.cursor()
+        self.cursor_sql_server = self.conn_sql_server.cursor()
+
         self.last_devices = []
         self.last_update_time = datetime.datetime.now()
 
-    def get_devices(self):
-        if self.current_os == 'Windows':
-            try:
-                import pywinusb.hid as hid
-                devices = hid.find_all_hid_devices()
-
-                for index, device in enumerate(devices):
-                    porta = f"Porta {index + 1}"
-                    produto = device.product_name if device.product_name else 'Desconhecido'
-                    fabricante = device.vendor_name if device.vendor_name else 'Desconhecido'
-
-                    # Inserir informações na tabela CodigoComponentes
-                    self.cursor.execute(
-                        "INSERT IGNORE INTO CodigoComponentes (Componente) VALUES (%s)",
-                        (porta,)
-                    )
-
-                    # Obter o ID do último componente inserido na tabela CodigoComponentes
-                    self.cursor.execute("SELECT LAST_INSERT_ID()")
-                    last_id = self.cursor.fetchone()[0]
-
-                    # Inserir informações na tabela DescricaoComponentes
-                    self.cursor.execute(
-    "INSERT IGNORE INTO DescricaoComponentes (produto, fabricante, dataDia ,fkComponente) VALUES (%s, %s, NOW(), %s)",
-    (produto, fabricante, last_id)
-)
-
-                self.conn.commit()
-                self.last_devices = devices
-                return devices
-            except ImportError:
-                print("A biblioteca pywinusb não está instalada. Por favor, instale-a para usar esta funcionalidade.")
-                return []
-    
-    def get_system_usage(self):
-        cpu_usage = psutil.cpu_percent()
-        ram_usage = psutil.virtual_memory().percent
-
+    def insert_or_get_id_from_codigo_componentes(self, componente):
         try:
-            # Inserir dados de CPU na tabela Leitura
-            self.cursor.execute(
-                "INSERT INTO Leitura (DataRegistro, Valor, Componente_ID, ATMComp_ID, APIID) VALUES (NOW(), %s, %s, %s, NULL)",
-                (cpu_usage, 3, 1)  
-            )
+            # Inserir ou obter ID do componente na tabela CodigoComponentes (SQL Server)
+            query_sql_server_cc = '''
+                IF NOT EXISTS (SELECT * FROM CodigoComponentes WHERE Componente = %s)
+                BEGIN
+                    INSERT INTO CodigoComponentes (Componente) VALUES (%s)
+                    SELECT SCOPE_IDENTITY()
+                END
+                ELSE
+                BEGIN
+                    SELECT idCodComponentes FROM CodigoComponentes WHERE Componente = %s
+                END
+            '''
+            self.cursor_sql_server.execute(query_sql_server_cc, (componente, componente, componente))
+            self.conn_sql_server.commit()
+            row = self.cursor_sql_server.fetchone()
+            if row:
+                return row[0]
+        except pymssql.Error as error:
+            print(f"Erro ao inserir ou obter ID do componente: {error}")
+        return None
 
-            # Inserir dados de RAM na tabela Leitura
-            self.cursor.execute(
-                "INSERT INTO Leitura (DataRegistro, Valor, Componente_ID, ATMComp_ID, APIID) VALUES (NOW(), %s, %s, %s, NULL)",
-                (ram_usage, 1, 1)  
-            )
+    def get_devices(self):
+        try:
+            devices = hid.find_all_hid_devices()
 
-            self.conn.commit()
-        except mysql.connector.Error as error:
-            print(f"Erro ao inserir na tabela Leitura: {error}")
+            for index, device in enumerate(devices):
+                porta = f"Porta {index + 1}"
+                produto = device.product_name if device.product_name else 'Desconhecido'
+                fabricante = device.vendor_name if device.vendor_name else 'Desconhecido'
 
-        return cpu_usage, ram_usage
+                # Obter ID do componente da tabela CodigoComponentes (SQL Server)
+                fk_componente = self.insert_or_get_id_from_codigo_componentes(porta)
+
+                if fk_componente is not None:
+                    # Inserir informações na tabela DescricaoComponentes (SQL Server)
+                    query_sql_server_dc = '''
+                        INSERT INTO DescricaoComponentes (produto, fabricante, dataDia, fkComponente) VALUES (%s, %s, GETDATE(), %s)
+                    '''
+                    self.cursor_sql_server.execute(query_sql_server_dc, (produto, fabricante, fk_componente))
+                    self.conn_sql_server.commit()
+
+            self.last_devices = devices
+            return devices
+        except ImportError:
+            print("A biblioteca pywinusb não está instalada. Por favor, instale-a para usar esta funcionalidade.")
+            return []
 
     def list_usb_devices(self):
         cpu_usage, ram_usage = self.get_system_usage()
@@ -92,30 +96,59 @@ class USBMonitor:
         print("Uso de RAM:")
         print(f"    {ram_usage}%")
 
+    def get_system_usage(self):
+        cpu_usage = psutil.cpu_percent()
+        ram_usage = psutil.virtual_memory().percent
+
+        try:
+            # Inserir dados de CPU na tabela Leitura (SQL Server)
+            query_sql_server_cpu = '''
+                INSERT INTO Leitura (DataRegistro, Valor, Componente_ID, ATMComp_ID, APIID) VALUES (GETDATE(), %s, %s, %s, NULL)
+            '''
+            self.cursor_sql_server.execute(query_sql_server_cpu, (cpu_usage, 3, 1))
+            self.conn_sql_server.commit()
+
+            # Inserir dados de RAM na tabela Leitura (SQL Server)
+            query_sql_server_ram = '''
+                INSERT INTO Leitura (DataRegistro, Valor, Componente_ID, ATMComp_ID, APIID) VALUES (GETDATE(), %s, %s, %s, NULL)
+            '''
+            self.cursor_sql_server.execute(query_sql_server_ram, (ram_usage, 1, 1))
+            self.conn_sql_server.commit()
+
+        except pymssql.Error as error:
+            print(f"Erro ao inserir na tabela Leitura (SQL Server): {error}")
+
+        return cpu_usage, ram_usage
+
     def run(self):
         print("Bem-vindo ao Monitor de Dispositivos!")
         while True:
             print("===================")
             print("Escolha uma opção:")
-            print("1 - Atualizar Lista de Dispositivos")
-            print("2 - Mostrar Dispositivos")
-            print("3 - Sair")
+            print("1 - Iniciar captura de dispositivos USB")
+            print("2 - Sair")
             escolha = input("Digite o número correspondente à opção desejada: ")
-            
+
             if escolha == '1':
-                self.get_devices()
-                self.last_update_time = datetime.datetime.now()
-                print("Lista Atualizada!")
+                resposta = input("Deseja ligar a captura de USB? (S/N): ")
+                if resposta.upper() == 'S':
+                    while True:
+                        self.get_devices()
+                        self.list_usb_devices()
+                        time.sleep(7)
+                else:
+                    print("Captura de USB não foi iniciada.")
             elif escolha == '2':
-                self.list_usb_devices()
-            elif escolha == '3':
                 print("Encerrando o monitor de dispositivos.")
                 break
             else:
                 print("Opção inválida. Tente novamente.")
 
-        self.cursor.close()
-        self.conn.close()
+        self.cursor_mysql.close()
+        self.conn_mysql.close()
+        self.cursor_sql_server.close()
+        self.conn_sql_server.close()
+
 
 if __name__ == "__main__":
     usb_monitor = USBMonitor()
